f9cee2e6c34af37d9d959891c976605f6bf32ef4
Oscar <oscarfernandoortegarojas@gmail.com>
2024-06-13 15:54:44 -0500

    coreccion del ejercicio uno, ejercicio funcional

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,10 +1,10 @@
 <?php
-$cantidad = readline("infrese cuntas camisas desea comprar:");
+$cantidad = readline("ingrese cuntas camisas desea comprar:");
 echo "cantidad de camisas compradas : $cantidad \n";
 $precio = 30000;
 $preciototal = ($cantidad * $precio);
-echo "precio de la compra: $precio \n";
+echo "precio de la compra: $preciototal \n";
 $descuento = ($cantidad >=3)? 0.20 : 0.10;
-$descuentototal = $preciototal ($preciototal + $descuento);
+$descuentototal = $preciototal - ($preciototal * $descuento);
 echo "se aplico un descuento y el valor de su compra es de: $descuentototal\n";
 ?>
